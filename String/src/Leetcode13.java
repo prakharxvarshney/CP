f969cf27e0622aded5b8326c58d1371fca6a1fe5
prakharxvarshney <prakhar.varshney200004@gmail.com>
2022-08-20 00:54:37 +0530

    Leetcode13

---
+++ String/src/Leetcode13.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class Leetcode13 {
+    public static int romanToInt(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int ans = map.get(s.charAt(s.length()-1));
+        for (int i=s.length()-2;i>=0;i--) {
+            if(map.containsKey(s.charAt(i))) {
+                if(map.get(s.charAt(i))<map.get(s.charAt(i+1))) {
+                    ans = ans-map.get(s.charAt(i));
+                }
+                else {
+                    ans = ans+map.get(s.charAt(i));
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+        System.out.println(romanToInt(s));
+    }
+}
