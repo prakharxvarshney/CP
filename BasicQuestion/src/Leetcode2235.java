13b58d1bc70b41a4482b63255e07f447f65f877c
prakharxvarshney <prakhar.varshney200004@gmail.com>
2022-08-19 19:08:25 +0530

    Leetcode2235

---
+++ BasicQuestion/src/Leetcode2235.java
@@ -0,0 +1,10 @@
+public class Leetcode2235 {
+    public static int sum(int num1, int num2) {
+        return num1+num2;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 12 ,num2=5;
+        System.out.println(sum(num1,num2));
+    }
+}
